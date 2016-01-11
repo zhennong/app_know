3e0fa0edaa128ce8a182c77e0e6e360cb4935f15
houpanqi <houpanqi@qq.com>
2016-01-11 16:34:08 +0800

    backup database

--- Application/User/Conf/config.php
+++ Application/User/Conf/config.php
@@ -15,5 +15,5 @@
 define('UC_APP_ID', 1); //应用ID
 define('UC_API_TYPE', 'Model'); //可选值 Model / Service
 define('UC_AUTH_KEY', 'OHSAa]*6XM`e&s5#:{b/=1!?.|g[KIUD9Yk3GdxJ'); //加密KEY
-define('UC_DB_DSN', 'mysqli://root:root@127.0.0.1:3306/app_know'); // 数据库连接，使用Model方式调用API必须配置此项
+define('UC_DB_DSN', 'mysqli://root:root@'.C('DB_HOST').':3306/app_know'); // 数据库连接，使用Model方式调用API必须配置此项
 define('UC_TABLE_PREFIX', 'app_'); // 数据表前缀，使用Model方式调用API必须配置此项
