36669bfd7b9f825cf47f5962f8116adb21d45fce
wodrow <wodrow451611cv@gmail.com>
2016-01-11 18:06:34 +0800

    Merge origin/master
    Conflicts:
    	Application/Home/Controller/IndexController.class.php

---
+++ Application/Home/Controller/ToolsController.class.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Home\Controller;
+
+/**
+ * Description of ToolsController
+ *
+ * @author wodrow
+ */
+class ToolsController extends HomeController{
+    public function plantcare() {
+        $this->display();
+    }
+}
